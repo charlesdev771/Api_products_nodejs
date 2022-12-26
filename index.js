4de882fc082a155422104f22afff302fe1f4d756
charlesdev771 <charlesdev07721@proton.me>
2022-12-26 03:38:14 -0300

    Final 1.0

--- index.js
+++ index.js
@@ -15,145 +15,187 @@
 
 //;
 
-app.get('/', (req, res) =>{
-    
-    try 
+
+//Routes with: CRUD
+    app.get('/', (req, res) =>
     {
-        res.json({ message: "Hello WOrld!" });
-    }
-    catch (error) 
-    {
-        res.json({message: error})    
-    }
-
-});
-
-
-app.post('/products', async (req, res) =>
-{
-
-    try 
-
-    {
-    
-        const { name, category, price } = req.body;
-
-        if (!name || !category || !price) 
-        {
-            res.status(422).json({ error: 'All camps are obrigatory!' });
-        }
-
-        const product = 
-        {
-            name, category, price
-        }
-
-        await Product.create(product);
-        res.status(201).json({message: "Success"});
-
-    } catch (error) 
-    {
-        console.log(error);    
-    }
-
-
-});
-
-app.get('/products', async (req, res) =>
-{
-    try 
-    {
-        const products = await Product.find();
-        res.status(200).json(products);
-    } 
-    catch (error) 
-    {
-        console.log(error)
-    }
-});
-
-app.get('/product/:id', async (req, res) =>
-{
-    try 
-    {
-        const id = req.params.id;
-        const product = await Product.findOne({_id: id});
         
-        if (!product)
+        try 
         {
-            res.status(422).json({message: 'Not found'});
-            return 
+            res.json({ message: "Hello World!" });
         }
 
-        res.status(200).json(product);
+        catch (error) 
+        {
+            res.json({message: error})
+            console.log(error)    
+        }
 
-    }
-    catch (error) 
+    });
+
+    app.post('/products', async (req, res) =>
+    
     {
-        console.log(error);
-    }
-});
 
+        try 
 
-app.patch('/product/:id', async (req, res)=> 
-{
-
-    try 
-    {
-        id = req.params.id;   
-        const {name, category, price} = req.body;
+            {
         
-        const products = 
+                const { name, category, price } = req.body;
+
+                if (!name || !category || !price) 
+                
+                {
+              
+                    res.status(422).json({ error: 'All camps are obrigatory!' });
+              
+                }
+
+            const products = 
+            {
+                name, category, price
+            }
+
+            await Product.create(products);
+            res.status(201).json({message: "Success"});
+
+        } 
+        catch (error) 
+        
         {
-            name,
-            category,
-            price,
+            
+            console.log(error);    
+        
         }
 
-        const update = await Product.updateOne({_id: id}, products);
 
-        if (update.matchedCount === 0)
+    });
+
+    app.get('/products', async (req, res) =>
+    {
+        try 
+
+        
         {
-            res.status(422).json({message: "Not found"});
 
-            return 
+            const products = await Product.find();
+            res.status(200).json(products);
+        
+        } 
+        catch (error) 
+        {
+
+            console.log(error)
+        
+        }
+    });
+
+    app.get('/product/:id', async (req, res) =>
+    {
+        try 
+        {
+
+            const id = req.params.id;
+            const product = await Product.findOne({_id: id});
+            
+            if (!product)
+            
+            {
+
+                res.status(422).json({message: 'Not found'});
+                return 
+            
+            }
+
+            res.status(200).json(product);
+
+        }
+        catch (error) 
+        {
+            console.log(error);
+        }
+    });
+
+
+    app.patch('/product/:id', async (req, res)=> 
+    
+    {
+
+        try 
+        
+        {
+            id = req.params.id;   
+            const {name, category, price} = req.body;
+            
+            const products = 
+            
+            {
+                name,
+                category,
+                price,
+            }
+
+            const update = await Product.updateOne({_id: id}, products);
+
+            if (update.matchedCount === 0)
+            
+            {
+            
+                res.status(422).json({message: "Not found"});
+                return 
+            
+            }
+
+            res.status(200).json(products);
+
+        }catch(error) 
+        {
+
+            console.log(error);
+        
         }
 
-        res.status(200).json(products);
+    });
 
-    }catch(error) 
+    app.delete("/product/:id", async (req, res) =>
+    
     {
-        res.status(500).json({erro: error})
-    }
-
-});
-
-app.delete("/product/:id", async (req, res) =>
-{
-    try 
-    {
-        const id = req.params.id;
-        const product = await Product.findOne({_id: id});
-
-        if (!product)
+        try 
+     
         {
-            res.status(422).json({message: 'not found'});
-            return
+            const id = req.params.id;
+            const product = await Product.findOne({_id: id});
+
+            if (!product)
+            {
+                res.status(422).json({message: 'not found'});
+                return
+            }
+
+            await Product.deleteOne({_id: id})
+            res.status(200).json({ message: 'Removed!' })
+   
+        } catch (error) 
+        {
+            console.log(error)    
         }
+    })
+//;
 
-        await Product.deleteOne({_id: id})
-        res.status(500).json({erro: 'Removed'});
-    } catch (error) 
+//Mongoose connect with main driver
+    mongoose.connect
+    (
+    
+        'mongodb+srv://ccod:eWqtVlff9dcTDiGQ@cluster0.oxfzeyg.mongodb.net/?retryWrites=true&w=majority'
+    
+    ).then(() => 
     {
-        console.log(error)    
-    }
-})
 
-mongoose.connect(
-    'mongodb+srv://ccod:eWqtVlff9dcTDiGQ@cluster0.oxfzeyg.mongodb.net/?retryWrites=true&w=majority'
-).then(() => 
-{
-    console.log("Its work!");
-    app.listen(7777)
-}).catch((err) => 
-    console.log(err))
+        console.log("Its work!");
+        app.listen(7777);
+
+    }).catch((err) => 
+        
+        console.log(err)
+    );
+//;
